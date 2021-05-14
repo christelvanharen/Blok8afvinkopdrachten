b3d8683fa068b9077a9bfb91e1a48ad4c2864c57
christelvanharen <NewOrleans@2019>
2021-05-14 16:26:19 +0200

    afvink 3 af

--- Afvinkopdracht3/afvink3.py
+++ Afvinkopdracht3/afvink3.py
@@ -1,52 +1,82 @@
-from Bio import Entrez
+from Bio import Entrez, Medline
 
-def search_terms():
-    term = ("")
-    # "Cypripedioideae [ab] AND matK [ab]",
+
+def search_terms(term):
+    """
+    Searching the PubMed with the query.
+    :param term: The given query.
+    :return: Count: for the amount of queries that has been found.
+    Id_list for the PudMed ID's.
+    """
     Entrez.email = "cca.vanharen@student.han.nl"
     handle = Entrez.esearch(db="pubmed", term=term, retmax=100)
-
-    # Reads the esearch and saves the ID's in id_list
     record = Entrez.read(handle)
     handle.close()
+    count = (record["Count"])
     id_list = (record["IdList"])
-    print(id_list)
 
-    return id_list
-
-def terms():
+    return count, id_list
 
 
-def get_pubtator_link(id_list):
-    """This function uses the id's from the id_list to create a pubtator link
-    which filters the genes, mutations and diseases out of the title and
-    abstract.
-
-    :param id_list: List with all found pubmed id's.
-    :return pubtator_link: Pubtator link with the title, abstact, genes,
-    diseases and mutations of each article.
+def making_query(compounds, genes, molecular):
     """
-    # Standard pubtator link format:
-    link = "https://www.ncbi.nlm.nih.gov/research/pubtator-api/publications" \
-           "/export/pubtator?pmids=idvalues&concepts=gene,mutation,disease"
+    Combining the 3 files into queries.
+    :param compounds: The compounds file
+    :param genes: The genes file
+    :param molecular: The molecular_effects file
+    :return: id_list_most could be used in the next function
+    """
+    for compound in compounds.split("\n"):
+        # print(compound)
+        for gen in genes.split("\n"):
+            # print(gen)
+            for molecule in molecular.split("\n"):
+                # print(molecule)
 
-    # Creates a string with all the ID's. Separated by a comma.
-    id_string = ""
-    for i in range(len(id_list)):
-        id_string += id_list[i] + ","
-    id_string = id_string[:-1]
+                query = "Query: (" + compound + " [tiab] AND " + gen + \
+                        " [tiab] AND " + molecule + " [tiab])"
+                print(query)
+                count, id_list = search_terms(query)
+                most = 0
+                id_list_most = []
+                if int(count) > most:
+                    id_list_most = id_list
+                get_article(id_list_most)
 
-    # Adds the ID values to the standard link.
-    pubtator_link = link.replace("idvalues", id_string)
-    print(pubtator_link)
 
-    return pubtator_link
+def get_article(id_list):
+    """
+    With the known PudMed ID's searching for the title, abstract and
+    the author of the article.
+    :param id_list: The list with the PubMed ID's
+    :return: The info list with the info of the article if there is a match.
+    """
+    for pubmed_id in id_list:
+        search_handle = Entrez.efetch(db="pubmed",
+                                      id=pubmed_id,
+                                      rettype="medline",
+                                      retmode="text",
+                                      sort="first+author")
+        records = list(Medline.parse(search_handle))
+        title = records[0].get("TI", "?")
+        abstract = records[0].get("AB", "?")
+        author = records[0].get("FAU", "?")
+        if title != "?" or abstract != "?" or author != "?":
+            info = [pubmed_id, title, abstract, author]
+            print(info)
+        else:
+            print("There is an item missing.")
+
 
 def main():
-    compounds = open("small_compounds.txt")
-    genes = open("small_genes.txt")
-    molecular = open("small_compounds.txt")
-    id_list = search_terms()
-    get_pubtator_link(id_list)
+    # "Cypripedioideae [ab] AND matK [ab]"
+    compounds = open("compounds.txt").read()
+    genes = open("genes.txt").read()
+    molecular = open("molecular_effects.txt").read()
+    making_query(compounds, genes, molecular)
+    count, id_list = search_terms("Cypripedioideae [tiab] AND matK ["
+                                  "tiab]")
+    get_article(id_list)
+
 
 main()
